405cbf6b4cd00de7510bf193143f205c13d57002
mohamedadel46 <muhammed195826@feng.bu.edu.eg>
2023-12-19 20:12:20 +0200

    some

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from AC import enforce_arc_consistency
 
 class SudokuCSP:
     size = 0 # size of the grid
@@ -82,7 +82,17 @@ def backtrack(csp):
                 return True
     return None
 
-
+sudoku_board9_9 = [
+    [0, 0, 0, 0, 0, 0, 6, 8, 0],
+    [0, 0, 0, 0, 7, 3, 0, 0, 9],
+    [3, 0, 9, 0, 0, 0, 0, 4, 5],
+    [4, 9, 0, 0, 0, 0, 0, 0, 0],
+    [8, 0, 3, 0, 5, 0, 9, 0, 2],
+    [0, 0, 0, 0, 0, 0, 0, 3, 6],
+    [9, 6, 0, 0, 0, 0, 3, 0, 8],
+    [7, 0, 0, 6, 8, 0, 0, 0, 0],
+    [0, 2, 8, 0, 0, 0, 0, 0, 0]
+]
 
 
 def main():
@@ -90,8 +100,12 @@ def main():
     #csmWithMRV_CP = SudokuCSP(sudoku_board9_9, select_unassigned_var = select_unassigned_var_MRV, inference = enforce_arc_consistency)
     #cspWithMRV_forward_check = SudokuCSP(sudoku_board9_9, select_unassigned_var = select_unassigned_var_MRV, inference = forward_check)
     #cspWithForwardCheck = SudokuCSP(sudoku_board9_9, inference = forward_check)
-    #cspWithCP = SudokuCSP(sudoku_board9_9, inference = enforce_arc_consistency)
-    #csp = SudokuCSP(sudoku_board9_9)
+    cspWithCP = SudokuCSP(sudoku_board9_9, inference = enforce_arc_consistency)
+    backtrack(cspWithCP)
+    print(cspWithCP.grid)
+    # csp = SudokuCSP(sudoku_board9_9)
     # todo compare performance of different algorithms
     return None
 
+if __name__ == "__main__":
+    main()
